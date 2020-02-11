838d8b40563d688f39e516ddb6630562bec983b4
pmouseguru <mrmouse.guru@gmail.com>
2020-02-12 00:08:20 +0700

    adding script statements for connecting from any host [ci skip]

--- src/main/scripts/configure-mysql.sql
+++ src/main/scripts/configure-mysql.sql
@@ -9,6 +9,10 @@ CREATE DATABASE pmg_prod;
 #Create database service accounts
 CREATE USER 'pmg_dev_user'@'localhost' IDENTIFIED BY 'pmouseguru';
 CREATE USER 'pmg_prod_user'@'localhost' IDENTIFIED BY 'pmouseguru';
+CREATE USER 'pmg_dev_user'@'%' IDENTIFIED BY 'pmouseguru';
+CREATE USER 'pmg_prod_user'@'%' IDENTIFIED BY 'pmouseguru';
+
+
 
 #Database grants
 GRANT SELECT ON pmg_dev.* to 'pmg_dev_user'@'localhost';
@@ -19,3 +23,12 @@ GRANT SELECT ON pmg_prod.* to 'pmg_prod_user'@'localhost';
 GRANT INSERT ON pmg_prod.* to 'pmg_prod_user'@'localhost';
 GRANT DELETE ON pmg_prod.* to 'pmg_prod_user'@'localhost';
 GRANT UPDATE ON pmg_prod.* to 'pmg_prod_user'@'localhost';
+
+GRANT SELECT ON pmg_dev.* to 'pmg_dev_user'@'%';
+GRANT INSERT ON pmg_dev.* to 'pmg_dev_user'@'%';
+GRANT DELETE ON pmg_dev.* to 'pmg_dev_user'@'%';
+GRANT UPDATE ON pmg_dev.* to 'pmg_dev_user'@'%';
+GRANT SELECT ON pmg_prod.* to 'pmg_prod_user'@'%';
+GRANT INSERT ON pmg_prod.* to 'pmg_prod_user'@'%';
+GRANT DELETE ON pmg_prod.* to 'pmg_prod_user'@'%';
+GRANT UPDATE ON pmg_prod.* to 'pmg_prod_user'@'%';
